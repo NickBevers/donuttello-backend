79cbc0ae92328aeb6e9fdac45bee4fee725ce1ff
nickbevers <nick.bevers9@gmail.com>
2022-10-31 20:42:03 +0100

    fix(main): change path for userController to go back one less folder

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../../controllers/userController');
+const userController = require('../controllers/userController');
 
 /* GET users listing. */
 router.get('/', userController.getAll);
