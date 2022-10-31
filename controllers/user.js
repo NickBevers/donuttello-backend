f279284ef867746e7cd575db762019c153aaebc9
nickbevers <nick.bevers9@gmail.com>
2022-10-31 13:16:38 +0100

    feat(main): add user related functionality (creating a user, signing in, removing an account) temporary validation with bcrypt instead of passport-local-mongoose

---
+++ controllers/user.js
@@ -0,0 +1,79 @@
+const User = require('../models/Users');
+
+// GET all users
+const getAll = (req, res) => {
+    User.find()
+        .then(users => res.json(users))
+        .catch(err => res.status(400).json('Error: ' + err));
+};
+
+// GET one user
+const getOne = (req, res) => {
+    User.findById(req.params.id)
+        .then(user => res.json(user))
+        .catch(err => res.status(400).json('Error: ' + err));
+};
+
+// POST create user
+const create = (req, res) => {
+    const newUser = new User({
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    newUser.save()
+        .then(() => res.json('User added!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+};
+
+// POST login user
+const login = (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email })
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({ emailnotfound: "Email not found" });
+            }
+
+            bcrypt.compare(password, user.password).then(match => {
+                if (match) {
+                    const payload = {
+                        id: user.id,
+                        name: user.name
+                    };
+
+                    jwt.sign(
+                        payload,
+                        keys.secretOrKey,
+                        {
+                            expiresIn: 31556926 
+                        },
+                        (err, token) => {
+                            res.json({
+                                success: true,
+                                token: "Bearer " + token
+                            });
+                        }
+                    );
+                } else {
+                    return res
+                        .status(400)
+                        .json({ passwordincorrect: "Password incorrect" });
+                }
+            });
+        });
+};
+
+// DELETE remove user
+const remove = (req, res) => {
+    User.findByIdAndDelete(req.params.id)
+        .then(() => res.json('User deleted.'))
+        .catch(err => res.status(400).json('Error: ' + err));
+};
+
+module.exports = { getAll, getOne, create, login, remove };
+
