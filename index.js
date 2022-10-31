50324ef22574225133ba8826bbd029788769c4d7
nickbevers <nick.bevers9@gmail.com>
2022-10-31 20:44:50 +0100

    feat(main): add mongodb connection with mongoose; feat(main): add comments for every block of code; fix(main): change all var to const

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Setup the dotenv package
+require('dotenv').config();
 
-var app = express();
+// Define the routes to the correct files
+const donutRouter = require('./routes/api/v1/donuts');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
+const app = express();
+
+// Setup extra settings for express
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Configure routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/v1/donuts', donutRouter);
+
+// Connect to the database
+mongoose.connect(process.env.DB_CONN, {useNewUrlParser: true, useUnifiedTopology: true});
 
 module.exports = app;
