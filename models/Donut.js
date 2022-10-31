0a595d8552d023a242cf573978d97c63c74eadaf
nickbevers <nick.bevers9@gmail.com>
2022-10-31 11:52:49 +0100

    feat: add Donut schema to define the needed fields for the collection

---
+++ models/Donut.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const donutSchema = new mongoose.Schema({
+    base: {String, required: true},
+    frosting: {String, required: true},
+    sprinkles: {String, required: true},
+    name: {String, required: true},
+    email: {String, required: true}
+})
+
+const Donut = mongoose.model('Donut', donutSchema);
+
+module.exports = Donut;
