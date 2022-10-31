122328bcf47178cd7f239c4ad6cb1533d974b543
nickbevers <nick.bevers9@gmail.com>
2022-10-31 11:13:41 +0100

    feat(main): add routes for all donut funcitons (get, post, put, delete) with donutController

---
+++ routes/api/v1/donut.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const donutController = require('../../controllers/donutController');
+
+/* GET donuts */
+router.get('/', donutController.getAll);
+router.get('/:id', donutController.getOne);
+
+/* POST new donut */
+router.post('/', donutController.create);
+
+/* PUT update donut */
+router.put('/:id', donutController.update);
+
+/* DELETE donut */
+router.delete('/:id', donutController.remove);
+
+module.exports = router;
