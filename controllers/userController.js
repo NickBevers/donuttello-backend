1d5992e0c9d2f60dfad226b7e073ea378f9d8470
nickbevers <nick.bevers9@gmail.com>
2022-11-01 11:19:27 +0100

    fix(userController): change user functions to match the donut functions in style (no .then but arrowfunction in the find statement

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,30 +3,32 @@ const bcrypt = require('bcrypt');
 
 // GET all users
 const getAll = (req, res) => {
-    User.find()
-        .then(users => res.json(users))
-        .catch(err => res.status(400).json('Error: ' + err));
+    User.find({}, (err, users) => {
+        if (err) {
+            res.status(404).json({ status: "failed", message: "Something has gone wrong.", error: err });
+        }
+        res.status(200).json({ status: "success", message: "All users retrieved.", data: users });
+    });
 };
 
 // GET one user
 const getOne = (req, res) => {
-    User.findById(req.params.id)
-        .then(user => res.json(user))
-        .catch(err => res.status(400).json('Error: ' + err));
+    User.findOne( req.params.id, (err, user) => {
+        if (err) {
+            res.status(404).json({ status: "failed", message: "Something has gone wrong.", error: err });
+        }
+        res.status(200).json({ status: "success", message: `Got data for user ${req.params.id}.`, data: user });
+    });
 };
 
 // POST create user
 const create = (req, res) => {
-    const newUser = new User({
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
-        email: req.body.email,
-        password: req.body.password
+    User.create(req.body, (err, user) => {
+        if (err) {
+            res.status(404).json({ status: "failed", message: "Something has gone wrong.", error: err });
+        }
+        res.status(200).json({ status: "success", message: `You created a user called "${user.name}".`, data: user });
     });
-
-    newUser.save()
-        .then(() => res.json('User added!'))
-        .catch(err => res.status(400).json('Error: ' + err));
 };
 
 // POST login user
@@ -34,46 +36,34 @@ const login = (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
 
-    User.findOne({ email })
-        .then(user => {
-            if (!user) {
-                return res.status(404).json({ emailnotfound: "Email not found" });
+    User.findOne({ email }, (err, user) => {
+        if (err) {
+            res.status(404).json({ status: "failed", message: "Something has gone wrong.", error: err });
+        }
+        if (!user) {
+            res.status(404).json({ status: "failed", message: "User not found." });
+        }
+        bcrypt.compare(password, user.password, (err, result) => {
+            if (err) {
+                res.status(404).json({ status: "failed", message: "Something has gone wrong.", error: err });
             }
-
-            bcrypt.compare(password, user.password).then(match => {
-                if (match) {
-                    const payload = {
-                        id: user.id,
-                        name: user.name
-                    };
-
-                    jwt.sign(
-                        payload,
-                        keys.secretOrKey,
-                        {
-                            expiresIn: 31556926 
-                        },
-                        (err, token) => {
-                            res.json({
-                                success: true,
-                                token: "Bearer " + token
-                            });
-                        }
-                    );
-                } else {
-                    return res
-                        .status(400)
-                        .json({ passwordincorrect: "Password incorrect" });
-                }
-            });
+            if (result) {
+                res.status(200).json({ status: "success", message: `You logged in as "${user.name}".`, data: user });
+            } else {
+                res.status(404).json({ status: "failed", message: "Wrong password." });
+            }
         });
+    });
 };
 
 // DELETE remove user
 const remove = (req, res) => {
-    User.findByIdAndDelete(req.params.id)
-        .then(() => res.json('User deleted.'))
-        .catch(err => res.status(400).json('Error: ' + err));
+    User.findByIdAndDelete(req.params.id, (err, user) => {
+        if (err) {
+            res.status(404).json({ status: "failed", message: "Something has gone wrong.", error: err });
+        }
+        res.status(200).json({ status: "success", message: `You deleted a user called "${user.name}".`, data: user });
+    });
 };
 
 module.exports = { getAll, getOne, create, login, remove };
