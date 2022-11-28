da52ab7fc48ff368b382e9b5b97c15682506d52d
nickbevers <nick.bevers9@gmail.com>
2022-11-28 21:35:31 +0100

    feat(tokenCheck): add check to make sure a token is present in the request

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -3,6 +3,11 @@ const jwt = require("jsonwebtoken");
 
 // Create a middleware function to check if the user is authenticated (has the correct token and email)
 module.exports = (req, res, next) => {
+    // Check if there is a token in the request
+    if(!req.headers.authorization) {
+        return res.status(401).json({ status: "failed", message: "You are not authorized to access this resource." });
+    }
+
     // Get the token from the request header
     const jwtToken = req.headers.authorization.split(" ")[1];
     if (!jwtToken) {
